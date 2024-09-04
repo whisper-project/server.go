762dd45b0e2c9f49d4940cfa9de4b7441bf282cf
Daniel Brotsky <dan@clickonetwo.io>
2024-09-03 23:29:31 -0700

    Cut in the gin framework.

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"clickonetwo/whisper/server/storage"
 	"log"
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+
+	"clickonetwo.io/whisper/server/storage"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func main() {
-	err := storage.LoadConfig()
+	err := storage.PushConfig(".env")
 	if err != nil {
 		log.Fatal("Can't load configuration: ", err)
 	}
+	defer storage.PopConfig()
+
+	// set up main router with zap logging
+	r := gin.New()
+	logger, _ := zap.NewDevelopment()
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
 }
